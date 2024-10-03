85026686a15cd49fd04e8b855e2cf57b0f2fc1af
iradtaufique <iradukunda.ta@gmail.com>
2024-10-02 18:07:35 -0700

    started working on creating Django custom command, this is a greating custom command

---
+++ dataentry/management/commands/greeting.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = "This greeting Command Will Great User"
+
+    """adding a method that will be parsed in kwarg parameter of handler method"""
+    def add_arguments(self, parser):
+        """parser will allow to run command with
+        extra argument on the command"""
+        parser.add_argument('name', type=str, help='Specify the User Name')
+
+    """Program Logic Entry"""
+    def handle(self, *args, **kwargs):
+        usrName = kwargs['name']
+        greeting = f'Hi {usrName}, Good Morning Welcome!'
+        self.stdout.write(greeting)
+
+        """show greeting as an Error"""
+        #self.stderr.write(greeting)
+        """show greeting as a success Message"""
+        #self.stdout.write(self.style.SUCCESS(greeting))
