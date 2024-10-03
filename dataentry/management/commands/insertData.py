97ca6ec15812f350029c37e4f3744a64083e80b7
iradtaufique <iradukunda.ta@gmail.com>
2024-10-03 14:08:11 -0700

    added validation check against data already existed in the database

--- dataentry/management/commands/insertData.py
+++ dataentry/management/commands/insertData.py
@@ -13,12 +13,26 @@ class Command(BaseCommand):
         dataset = [
             {'roll_no': 1001, 'name': 'John', 'age': 10},
             {'roll_no': 1002, 'name': 'Doe', 'age': 11},
-            {'roll_no': 1003, 'name': 'Peter', 'age': 12},
+            {'roll_no': 1005, 'name': 'Hope', 'age': 15},
             {'roll_no': 1004, 'name': 'Cyusa', 'age': 13},
+            {'roll_no': 1006, 'name': 'Kate', 'age': 16},
         ]
 
+        #todo: creating a variables that will count records are existed and new records
+        new_rec_num =0
+        exst_rec_num =0
         for data in dataset:
-            #todo: insert this data in student table
-            Students.objects.create(roll_no=data['roll_no'], name=data['name'], age=data['age'])
+            #todo: adding validation check
+            reg_no = data['roll_no']
+            existing_record = Students.objects.filter(roll_no=reg_no).exists()
 
-        self.stdout.write(self.style.SUCCESS('Data Entered Successfully'))
+            if not existing_record:
+                #todo: insert this data in student table
+                new_rec_num += 1
+                Students.objects.create(roll_no=data['roll_no'], name=data['name'], age=data['age'])
+
+            else:
+                exst_rec_num += 1
+                self.stdout.write(self.style.WARNING(f"User with this ID {data['roll_no']} already exists!"))
+
+        self.stdout.write(self.style.SUCCESS(f"Process Ended Successfully {new_rec_num} Entered in database and {exst_rec_num} Already Existed!"))
